b45783d659f4fddee328d561a04badbdc2f9dad8
Yichuan Shen <shen.yichuan@gmail.com>
2019-06-20 16:52:41 +0200

    Add SmoothInterpolatingPath component

---
+++ ui/component/SmoothInterpolatingPath.js
@@ -0,0 +1,18 @@
+import {h, Component} from 'preact'
+import {getMonotoneCubicInterpolation} from '../interpolation';
+
+export default class SmoothInterpolatingPath extends Component {
+  render() {
+    let {xs, ys, innerProps = {}} = this.props
+    let interpolation = getMonotoneCubicInterpolation(xs, ys)
+    let [xStart, xEnd] = [xs[0], xs.slice(-1)[0]]
+
+    return <path
+      {...innerProps}
+
+      d={[...Array((xEnd - xStart) + 1)].map((_, i) => xStart + i).map((x, i) =>
+        `${i === 0 ? 'M' : 'L'}${x},${interpolation(x)}`
+      ).join(' ')}
+    />
+  }
+}
