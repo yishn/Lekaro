7b1f512bdca43a23b6195ecae6ec56194e4865cc
Yichuan Shen <shen.yichuan@gmail.com>
2019-06-27 21:11:35 +0200

    Increase step size for SmoothInterpolatingCurve

--- ui/component/SmoothInterpolatingCurve.js
+++ ui/component/SmoothInterpolatingCurve.js
@@ -4,15 +4,16 @@ import {getMonotoneCubicInterpolation} from '../interpolation.js'
 export default class SmoothInterpolatingCurve extends Component {
   render() {
     try {
-      let {xs, ys, additionalPoints = [], innerProps = {}} = this.props
+      let {xs, ys, step = 6, additionalPoints = [], innerProps = {}} = this.props
       let interpolation = getMonotoneCubicInterpolation(xs, ys)
       let [xStart, xEnd] = [xs[0], xs.slice(-1)[0]]
+      let count = Math.ceil((xEnd - xStart) / step) + 1
 
       return <path
         {...innerProps}
 
         d={
-          [...Array(Math.ceil(xEnd - xStart) + 1)].map((_, i) => xStart + i).map((x, i) =>
+          [...Array(count)].map((_, i) => Math.min(xStart + step * i, xEnd)).map((x, i) =>
             `${i === 0 ? 'M' : 'L'}${x},${interpolation(x)}`
           ).join(' ') + ' ' + additionalPoints.map(([x, y]) =>
             `L${x},${y}`
