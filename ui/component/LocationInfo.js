aba4df29c51a13b59d6da2c730f20f0225d7cb26
Yichuan Shen <shen.yichuan@gmail.com>
2020-03-05 17:58:26 +0100

    Move rest of components to hooks

--- ui/component/LocationInfo.js
+++ ui/component/LocationInfo.js
@@ -1,124 +1,101 @@
-import {h, Component} from 'preact'
+import {h} from 'preact'
+import {useState, useEffect, useCallback} from 'preact/hooks'
 import classnames from 'classnames'
 import CurrentLocationIcon from '../resources/location.svg'
 
-export default class LocationInfo extends Component {
-  constructor(props) {
-    super(props)
+export default function LocationInfo({
+  inputRef,
+  units,
+  loading,
+  city,
+  state,
+  country,
 
-    this.state = {
-      prevLoading: false,
-      prevCity: null,
-      prevState: null,
-      prevCountry: null,
-      input: ''
-    }
-  }
+  onSearch = () => {},
+  onCurrentLocationClick = () => {},
+  onUnitsButtonClick = () => {}
+}) {
+  let [input, setInput] = useState('')
 
-  static getDerivedStateFromProps(props, state) {
-    let result = {
-      prevLoading: props.loading,
-      prevCity: props.city,
-      prevState: props.state,
-      prevCountry: props.country
-    }
-
-    if (
-      props.city !== state.prevCity ||
-      props.state !== state.prevState ||
-      props.country !== state.prevCountry ||
-      (!props.loading && state.prevLoading)
-    ) {
-      result = {
-        ...result,
-        input: props.city || props.state || props.country
+  useEffect(
+    function updateInput() {
+      if (!loading) {
+        setInput(city || state || country)
       }
-    }
+    },
+    [loading, city, state, country]
+  )
 
-    return result
-  }
+  return (
+    <div class={classnames('location-info', {loading})}>
+      <h2>
+        <input
+          ref={inputRef}
+          disabled={loading}
+          value={input}
+          placeholder="Location"
+          autofocus={!input}
+          onInput={useCallback(evt => {
+            setInput(evt.currentTarget.value)
+          }, [])}
+          onKeyDown={useCallback(
+            evt => {
+              if (evt.key === 'Enter') {
+                onSearch({value: input})
+              }
+            },
+            [input]
+          )}
+          onBlur={useCallback(
+            evt => {
+              if (!loading) {
+                setInput(city || state || country)
+              }
+            },
+            [loading, city, state, country]
+          )}
+        />
+      </h2>
 
-  handleInputInput = evt => {
-    this.setState({
-      input: evt.currentTarget.value
-    })
-  }
+      <div class="toolbar">
+        <a
+          class="currentlocation"
+          href="#"
+          title="Current Location"
+          onClick={useCallback(
+            evt => {
+              evt.preventDefault()
+              onCurrentLocationClick(evt)
+            },
+            [onCurrentLocationClick]
+          )}
+        >
+          <CurrentLocationIcon viewBox="0 0 24 24" />
+        </a>
 
-  handleInputKeyDown = evt => {
-    if (evt.key === 'Enter') {
-      let {onSearch = () => {}} = this.props
-      onSearch({value: this.state.input})
-    }
-  }
+        <h3>{[state, country].filter(x => !!x).join(', ') || 'Unknown'}</h3>
 
-  handleInputBlur = () => {
-    if (!this.props.loading) {
-      this.setState({
-        input: this.props.city || this.props.state || this.props.country
-      })
-    }
-  }
-
-  handleCurrentLocationClick = evt => {
-    evt.preventDefault()
-
-    let {onCurrentLocationClick = () => {}} = this.props
-    onCurrentLocationClick(evt)
-  }
-
-  handleUnitsButtonClick = evt => {
-    evt.preventDefault()
-
-    let {onUnitsButtonClick = () => {}} = this.props
-    onUnitsButtonClick(evt)
-  }
-
-  render() {
-    let {inputRef, units, loading, state, country} = this.props
-
-    return (
-      <div class={classnames('location-info', {loading})}>
-        <h2>
-          <input
-            ref={inputRef}
-            disabled={loading}
-            value={this.state.input}
-            placeholder="Location"
-            autofocus={!this.state.input}
-            onInput={this.handleInputInput}
-            onKeyDown={this.handleInputKeyDown}
-            onBlur={this.handleInputBlur}
-          />
-        </h2>
-
-        <div class="toolbar">
+        <div class="options">
           <a
-            class="currentlocation"
+            class="units"
             href="#"
-            title="Current Location"
-            onClick={this.handleCurrentLocationClick}
+            title="Change Units"
+            onClick={useCallback(
+              evt => {
+                evt.preventDefault()
+                onUnitsButtonClick(evt)
+              },
+              [onUnitsButtonClick]
+            )}
           >
-            <CurrentLocationIcon viewBox="0 0 24 24" />
+            {units === 'si'
+              ? 'Metric'
+              : units === 'us'
+              ? 'Imperial'
+              : 'Unknown'}
           </a>
-
-          <h3>{[state, country].filter(x => !!x).join(', ') || 'Unknown'}</h3>
-
-          <div class="options">
-            <a
-              class="units"
-              href="#"
-              title="Change Units"
-              onClick={this.handleUnitsButtonClick}
-            >
-              {units === 'si'
-                ? 'Metric'
-                : units === 'us'
-                ? 'Imperial'
-                : 'Unknown'}
-            </a>
-          </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
--- ui/component/WeatherDetails.js
+++ ui/component/WeatherDetails.js
@@ -1,4 +1,5 @@
 import {h, Component} from 'preact'
+import {useState, useEffect} from 'preact/hooks'
 import DegreeIcon from '../resources/degree.svg'
 import AreaIcon from '../resources/area.svg'
 import OzoneIcon from '../resources/ozone.svg'
@@ -11,161 +12,148 @@ import SnowIcon from '../resources/snow.svg'
 import SleetIcon from '../resources/sleet.svg'
 import AccumulationIcon from '../resources/accumulation.svg'
 
-export default class WeatherDetails extends Component {
-  constructor(props) {
-    super(props)
+export default function WeatherDetails({
+  units,
+  temperature,
+  apparentTemperature,
+  dewPoint,
+  cloudCover,
+  humidity,
+  ozone,
+  pressure,
+  windBearing: windBearingProp,
+  windSpeed,
+  windGust,
+  visibility,
+  precipProbability,
+  precipType,
+  precipIntensity,
+  precipAccumulation
+}) {
+  let [windBearing, setWindBearing] = useState(0)
+  let capitalize = str => str[0].toUpperCase() + str.slice(1).toLowerCase()
+  let round = (x, n = 0) => Math.round(x * 10 ** n) / 10 ** n
 
-    this.state = {
-      windBearing: 0
+  useEffect(function updateWindBearing() {
+    let newWindBearing = windBearingProp
+
+    while (Math.abs(windBearing - newWindBearing) > 180) {
+      newWindBearing =
+        newWindBearing + Math.sign(windBearing - newWindBearing) * 360
     }
-  }
 
-  static getDerivedStateFromProps(props, state) {
-    let {windBearing} = props
+    setWindBearing(newWindBearing)
+  })
 
-    while (Math.abs(state.windBearing - windBearing) > 180) {
-      let sign = Math.sign(state.windBearing - windBearing)
-
-      windBearing = windBearing + sign * 360
-    }
-
-    return {windBearing}
-  }
-
-  render() {
-    let {
-      units,
-      temperature,
-      apparentTemperature,
-      dewPoint,
-      cloudCover,
-      humidity,
-      ozone,
-      pressure,
-      windSpeed,
-      windGust,
-      visibility,
-      precipProbability,
-      precipType,
-      precipIntensity,
-      precipAccumulation
-    } = this.props
-
-    let {windBearing} = this.state
-    let capitalize = str => str[0].toUpperCase() + str.slice(1).toLowerCase()
-    let round = (x, n = 0) => Math.round(x * 10 ** n) / 10 ** n
-
-    return (
-      <div class="weather-details">
-        <ul>
-          <li class="temperature" title="Temperature">
-            <DegreeIcon class="icon" />
-            <span class="text">
-              {round(temperature, 1)} {units.temperature}
-            </span>
-          </li>
-          <li class="apparent" title="Perceived Temperature">
-            <DegreeIcon class="icon" />
-            <span class="text">
-              {round(apparentTemperature, 1)} {units.apparentTemperature}
-            </span>
-          </li>
-          <li class="dewpoint" title="Dew Point">
-            <DegreeIcon class="icon" />
-            <span class="text">
-              {round(dewPoint, 1)} {units.dewPoint}
-            </span>
-          </li>
-        </ul>
-        <ul>
-          <li class="cloudcover" title="Cloud Cover">
-            <AreaIcon class="icon" />
-            <span class="text">{round(cloudCover * 100)}%</span>
-          </li>
-          <li
-            class="precipitation"
-            title={`${
-              precipType != null ? capitalize(precipType) : 'Precipitation'
-            } Probability`}
-          >
-            <AreaIcon class="icon" />
-            <span class="text">{round(precipProbability * 100)}%</span>
-          </li>
-          <li class="humidity" title="Relative Humidity">
-            <AreaIcon class="icon" />
-            <span class="text">{round(humidity * 100)}%</span>
-          </li>
-        </ul>
-        <ul>
-          <li
-            title={`${
-              precipType != null ? capitalize(precipType) : 'Precipitation'
-            } Intensity`}
-          >
-            {precipType === 'snow' ? (
-              <SnowIcon class="icon" />
-            ) : precipType === 'sleet' ? (
-              <SleetIcon class="icon" />
-            ) : (
-              <RainIcon class="icon" />
-            )}
-            <span class="text">
-              {precipType != null ? round(precipIntensity, 2) : '-'}{' '}
-              {precipType != null && units.precipitation.intensity}
-            </span>
-          </li>
-          {precipAccumulation != null && (
-            <li title="Snow Accumulation">
-              <AccumulationIcon class="icon" />
-              <span class="text">
-                {round(precipAccumulation, 2)}{' '}
-                {units.precipitation.accumulation}
-              </span>
-            </li>
+  return (
+    <div class="weather-details">
+      <ul>
+        <li class="temperature" title="Temperature">
+          <DegreeIcon class="icon" />
+          <span class="text">
+            {round(temperature, 1)} {units.temperature}
+          </span>
+        </li>
+        <li class="apparent" title="Perceived Temperature">
+          <DegreeIcon class="icon" />
+          <span class="text">
+            {round(apparentTemperature, 1)} {units.apparentTemperature}
+          </span>
+        </li>
+        <li class="dewpoint" title="Dew Point">
+          <DegreeIcon class="icon" />
+          <span class="text">
+            {round(dewPoint, 1)} {units.dewPoint}
+          </span>
+        </li>
+      </ul>
+      <ul>
+        <li class="cloudcover" title="Cloud Cover">
+          <AreaIcon class="icon" />
+          <span class="text">{round(cloudCover * 100)}%</span>
+        </li>
+        <li
+          class="precipitation"
+          title={`${
+            precipType != null ? capitalize(precipType) : 'Precipitation'
+          } Probability`}
+        >
+          <AreaIcon class="icon" />
+          <span class="text">{round(precipProbability * 100)}%</span>
+        </li>
+        <li class="humidity" title="Relative Humidity">
+          <AreaIcon class="icon" />
+          <span class="text">{round(humidity * 100)}%</span>
+        </li>
+      </ul>
+      <ul>
+        <li
+          title={`${
+            precipType != null ? capitalize(precipType) : 'Precipitation'
+          } Intensity`}
+        >
+          {precipType === 'snow' ? (
+            <SnowIcon class="icon" />
+          ) : precipType === 'sleet' ? (
+            <SleetIcon class="icon" />
+          ) : (
+            <RainIcon class="icon" />
           )}
-        </ul>
-        <ul>
-          <li title="Wind Speed">
-            <WindIcon
-              class="icon"
-              style={{transform: `rotate(${windBearing}deg)`}}
-            />
+          <span class="text">
+            {precipType != null ? round(precipIntensity, 2) : '-'}{' '}
+            {precipType != null && units.precipitation.intensity}
+          </span>
+        </li>
+        {precipAccumulation != null && (
+          <li title="Snow Accumulation">
+            <AccumulationIcon class="icon" />
             <span class="text">
-              {round(windSpeed, 2)} {units.windSpeed}
+              {round(precipAccumulation, 2)} {units.precipitation.accumulation}
             </span>
           </li>
-          <li title="Wind Gust Speed">
-            <GustIcon class="icon" />
-            <span class="text">
-              {round(windGust, 2)} {units.windGust}
-            </span>
-          </li>
-        </ul>
-        <ul>
-          <li title="Sea-Level Air Pressure">
-            <PressureIcon class="icon" />
-            <span class="text">
-              {round(pressure, 2)} {units.pressure}
-            </span>
-          </li>
-          <li title="Columnar Density of Total Atmospheric Ozone">
-            <OzoneIcon class="icon" />
-            <span class="text">
-              {round(ozone, 2)} {units.ozone}
-            </span>
-          </li>
-          <li title="Average Visibility">
-            <VisibilityIcon class="icon" />
-            <span class="text">
-              {((units.visibility === 'km' && visibility >= 16.093) ||
-              (units.visibility === 'mi' && visibility >= 10)
-                ? '≥'
-                : '') + round(visibility, 2)}{' '}
-              {units.visibility}
-            </span>
-          </li>
-        </ul>
-      </div>
-    )
-  }
+        )}
+      </ul>
+      <ul>
+        <li title="Wind Speed">
+          <WindIcon
+            class="icon"
+            style={{transform: `rotate(${windBearing}deg)`}}
+          />
+          <span class="text">
+            {round(windSpeed, 2)} {units.windSpeed}
+          </span>
+        </li>
+        <li title="Wind Gust Speed">
+          <GustIcon class="icon" />
+          <span class="text">
+            {round(windGust, 2)} {units.windGust}
+          </span>
+        </li>
+      </ul>
+      <ul>
+        <li title="Sea-Level Air Pressure">
+          <PressureIcon class="icon" />
+          <span class="text">
+            {round(pressure, 2)} {units.pressure}
+          </span>
+        </li>
+        <li title="Columnar Density of Total Atmospheric Ozone">
+          <OzoneIcon class="icon" />
+          <span class="text">
+            {round(ozone, 2)} {units.ozone}
+          </span>
+        </li>
+        <li title="Average Visibility">
+          <VisibilityIcon class="icon" />
+          <span class="text">
+            {((units.visibility === 'km' && visibility >= 16.093) ||
+            (units.visibility === 'mi' && visibility >= 10)
+              ? '≥'
+              : '') + round(visibility, 2)}{' '}
+            {units.visibility}
+          </span>
+        </li>
+      </ul>
+    </div>
+  )
 }
